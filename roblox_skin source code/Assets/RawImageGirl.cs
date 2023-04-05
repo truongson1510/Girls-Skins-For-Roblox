5c8d6f50c0c0b9b33f1bd7a4ce608024e2fdf315
truongson1510 <50595413+truongson1510@users.noreply.github.com>
2023-04-06 02:30:29 +0700

    * Thêm tính năng xoay nhân vật xung quanh

---
+++ roblox_skin source code/Assets/RawImageGirl.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RawImage))]
+public class RawImageGirl : MonoBehaviour, IPointerDownHandler, IPointerMoveHandler, IPointerUpHandler
+{
+    #region Inspector Variables
+
+    [SerializeField] 
+    private Transform   girlTransform;
+
+    #endregion
+
+    #region Member Variables
+
+    private RawImage    rawImage;
+    private bool        isClickOnImage;
+
+    #endregion
+
+    #region Properties
+
+    #endregion
+
+    #region Unity Methods
+
+    private void Awake()
+	{
+		rawImage = GetComponent<RawImage>();
+	}
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+        isClickOnImage = true;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnPointerMove(PointerEventData eventData)
+    {
+        if (isClickOnImage)
+        {
+            float rotation = Input.GetAxis("Horizontal") * -0.25f;
+            girlTransform.Rotate(0f, rotation, 0f);
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+        isClickOnImage = false;
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    #endregion
+
+    #region Protected Methods
+
+    #endregion
+
+    #region Private Methods
+
+    #endregion
+}
