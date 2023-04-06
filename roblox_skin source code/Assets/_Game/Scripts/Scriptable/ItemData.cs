4a075f8871a0dd9eec1d760af0f005212dff6b65
truongson1510 <50595413+truongson1510@users.noreply.github.com>
2023-04-06 07:00:18 +0700

    * Làm data cho các item

---
+++ roblox_skin source code/Assets/_Game/Scripts/Scriptable/ItemData.cs
@@ -0,0 +1,49 @@
+
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public enum ItemType
+{
+	Face,
+	Shirt,
+	Hair,
+	Pant,
+	Glasses,
+	Hat
+}
+
+[CreateAssetMenu(menuName = "Data/ItemData")]
+public class ItemData : ScriptableObject
+{
+	#region Inspector Variables
+
+	public ItemType itemType;
+	public Sprite	itemSprite;
+	public Material itemMaterial;
+
+	#endregion
+
+	#region Member Variables
+
+	#endregion
+
+	#region Properties
+
+	#endregion
+
+	#region Unity Methods
+
+	#endregion
+
+	#region Public Methods
+
+	#endregion
+
+	#region Protected Methods
+
+	#endregion
+
+	#region Private Methods
+
+	#endregion
+}
